9f9056fe0c19f735fe07d7c3142c8fdf22d82105
yixy-only <yixy@xege.org>
2024-02-15 20:17:52 +0800

    adjust: 项目由多文件改为单文件，方便编译运行

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,177 @@
-#include <time.h>
 #include <graphics.h>
-#include <Windows.h>
-#include <stdio.h>
 
-#include "fireworks.h"
+#include <time.h>
+#include <math.h>
+#include <Windows.h>
+
+#define GROUND 580  //地面位置y坐标
+
+// 速度
+struct Speed
+{
+    double x, y;
+};
+
+// 位置
+struct Pos
+{
+    double x, y;
+};
+
+// 粒子
+struct Particle
+{
+    Pos pos;
+    Speed speed;
+};
+
+// 烟花
+class Fireworks
+{
+private:
+    static const int NUM_PARTICLE = 200;
+    static const double particleSpeed;
+    Particle p[NUM_PARTICLE];
+
+    color_t color;
+
+    int delayTime;      //延迟时间
+    int riseTime;       //上升时间
+    int bloomTime;      //爆炸时间
+
+    Pos risePos;        //上升阶段位置
+    Speed riseSpeed;    //上升速度
+
+public:
+    //初始化
+    Fireworks();
+
+    void init();
+
+    //更新位置等相关属性
+    void update();
+
+    //根据属性值绘画
+    void draw(PIMAGE pimg = NULL);
+};
+
+const double Fireworks::particleSpeed = 3.0;
+
+Fireworks::Fireworks()
+{
+    init();
+}
+
+void Fireworks::init()
+{
+    delayTime = rand() % 300 + 20;
+    riseTime = rand() % 80 + 160;
+    bloomTime = 160;
+
+    risePos.x = rand() % 450 + 300.0f;
+    risePos.y = GROUND;
+
+    riseSpeed.y = randomf() * 1.0 - 3.0; //速度 y: [-3.0, -2.0), 根据坐标系需要是负的
+    riseSpeed.x = randomf() * 0.4 - 0.2; //速度 x: [-0.2, 0.2), 可稍微倾斜
+
+    //随机颜色
+    color = HSVtoRGB((float)randomf() * 360.0, 1.0f, 1.0f);
+
+    //给每一个粒子设置初始速度
+    for (int i = 0; i < NUM_PARTICLE - 1; i += 2)
+    {
+        //为了球状散开，设初始速度大小相等
+        //初始随机速度水平角度和垂直角度，因为看到是平面的，所以求x, y分速度
+        double levelAngle = randomf() * 360;
+        double verticalAngle = randomf() * 360;
+
+        //速度投影到xOy平面
+        double xySpeed = particleSpeed * cos(verticalAngle);
+
+        //求x, y分速度
+        p[i].speed.x = xySpeed * cos(levelAngle);
+        p[i].speed.y = xySpeed * sin(levelAngle);
+
+        //动量守恒，每对速度反向
+        if (i + 1 < NUM_PARTICLE) {
+            p[i + 1].speed.x = -p[i].speed.x;
+            p[i + 1].speed.y = -p[i].speed.y;
+        }
+    }
+}
+
+void Fireworks::draw(PIMAGE pimg)
+{
+    //未开始
+    if (delayTime > 0)
+        return;
+    //烟花上升阶段
+    else if (riseTime > 0) {
+        setfillcolor(color, pimg);
+        //画四个点，这样大一些
+        bar(risePos.x, risePos.y, risePos.x + 2, risePos.y + 2, pimg);
+    }
+    //烟花绽放阶段
+    else {
+        setfillcolor(color, pimg);
+        for (int i = 0; i < NUM_PARTICLE; i++) {
+            bar(p[i].pos.x, p[i].pos.y, p[i].pos.x + 2, p[i].pos.y + 2, pimg);
+        }
+    }
+}
+
+//更新位置等相关属性
+void Fireworks::update()
+{
+    if (delayTime-- > 0)
+        return;
+    //处于上升阶段，只更新烟花位置
+    else if (riseTime > 0) {
+        risePos.x += riseSpeed.x;
+        risePos.y += riseSpeed.y;
+
+        //重力作用
+        riseSpeed.y += 0.005;
+
+        //上升完毕，到达爆炸阶段
+        if (--riseTime <= 0) {
+            //设粒子初始位置为烟花当前位置
+            for (int i = 0; i < NUM_PARTICLE; i++) {
+                p[i].pos.x = risePos.x;
+                p[i].pos.y = risePos.y;
+            }
+        }
+    }
+    //烟花绽放阶段
+    else if (bloomTime-- > 0) {
+        //粒子散开，更新粒子位置
+        for (int i = 0; i < NUM_PARTICLE; i++) {
+            p[i].pos.x += p[i].speed.x;
+            p[i].pos.y += p[i].speed.y;
+
+            //重力作用
+            p[i].speed.y += 0.005;
+
+            //速度减慢
+            p[i].speed.x *= 0.982;
+            p[i].speed.y *= 0.982;
+        }
+    }
+    else {
+        //烟花重新开始
+        init();
+    }
+}
+
 
 #define NUM_FIREWORKS 12    //烟花数量，12左右比较好，多了太密集
 
 int main()
 {
+
     initgraph(800, 800, INIT_RENDERMANUAL);
 
-    srand((unsigned)time(NULL));
-
-    char curDirPath[MAX_PATH];
-    GetCurrentDirectoryA(MAX_PATH, curDirPath);
-    printf("current directory:%s\n", curDirPath);
+    randomize();
 
     //烟花
     Fireworks* fireworks = new Fireworks[NUM_FIREWORKS];
